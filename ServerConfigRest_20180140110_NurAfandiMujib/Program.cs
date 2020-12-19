989d0dcc7368b311fc1c64b5000418f924ade350
Nur Afandi Mujib <fndmujib@gmail.com>
2020-12-19 23:01:43 +0700

    Add project files.

---
+++ ServerConfigRest_20180140110_NurAfandiMujib/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceRest_20180140110_NurAfandiMujib;
+
+namespace ServerConfigRest_20180140110_NurAfandiMujib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("Server Ready ...");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
